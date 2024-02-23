0bc3af74a257d293d55afe0215bf24e1b872b8c3
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-02-23 17:18:45 -0500

    Created a middleware and util function

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs';
+import {errorHandler} from "../utils/error.js"
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); //10 is salt number 
     const newUser = new User({username, email, password: hashedPassword});
@@ -10,7 +11,7 @@ export const signup = async(req,res)=>{
         res.status(201).json("User created succesfully")
 
     }catch(err){
-        res.status(500).json(err.message);
+        next(err);
 
     }
 };
--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import authRouter from "./routes/authRoute.js"
+import authRouter from "./routes/authRoute.js";
+import { err } from "./middlewares/authMiddleware.js";
 dotenv.config()
 
 
@@ -21,5 +22,12 @@ app.listen(3000, ()=>{
     console.log("Server is running in port 3000")
 })
 
+
+//Middleware
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+
+
+//Middleware
+app.use(err)
