1f644b5ea977c56c0531a0bdd9688449cbd6e8da
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-02-24 17:51:31 -0500

    added redux for user state

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -20,12 +20,16 @@ export const signin = async(req,res,next)=>{
     const {email, password} = req.body;
     try{
         const validUser = await User.findOne({email});
-        if(!validUser) return next(errorHandler(404, "Email Not Found"));
+        if(!validUser){
+            return next(errorHandler(404, "Email Not Found"));
+        }
         
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+        if(!validPassword){
+            return next(errorHandler(401, "Wrong Credentials"));
+        } 
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-        const{ password:pass, ...rest} = validUser._doc;
+        const{ password:pass, ...rest} = validUser._doc;  //here we are collecting the password property and renaming it to pass and we are collecting remainig all to rest
 
         res.cookie('access_token', token, {httpOnly:true}).status(200)
         .json({rest, access_token:token});
