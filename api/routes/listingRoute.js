16abc2953411d56eeca72141ad42f3b4b75fe4e3
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-03-09 17:15:26 -0500

    added image swiping

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing } from '../controllers/listingController.js';
+import { createListing, deleteListing, updateListing,getListing} from '../controllers/listingController.js';
 import {verifyToken} from "../utils/verifyUser.js"
 const listingRouter = express.Router();
 
 listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken , deleteListing);
-listingRouter.post("/update/:id", verifyToken, updateListing)
+listingRouter.post("/update/:id", verifyToken, updateListing);
+listingRouter.get('/get/:id', getListing);
 
 export default listingRouter;
