a2528d04b4b71f3662a71ced1ed35e6b266044ad
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-03-11 18:37:31 -0400

    added search API route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing,getListing} from '../controllers/listingController.js';
+import { createListing, deleteListing, updateListing,getListing, getListings} from '../controllers/listingController.js';
 import {verifyToken} from "../utils/verifyUser.js"
 const listingRouter = express.Router();
 
@@ -7,5 +7,6 @@ listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken , deleteListing);
 listingRouter.post("/update/:id", verifyToken, updateListing);
 listingRouter.get('/get/:id', getListing);
+listingRouter.get("/get", getListings);
 
 export default listingRouter;
