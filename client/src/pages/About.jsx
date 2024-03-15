39f8a510ae286519d3517c6365625ae9fb3c9245
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-03-15 17:24:04 -0400

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About CityScape</h1>
+      <div className="mb-4 text-slate-700">
+      Welcome to CityScape, your premier destination for buying and selling real estate properties! Whether you're searching for your dream home, looking to invest in lucrative opportunities, or selling your property hassle-free, we've got you covered.
+      </div>
+      <p>
+        
+      </p>
+    </div>
+ 
+    
+    </>
   )
 }
